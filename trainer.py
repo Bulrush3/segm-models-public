76bad742fda708a39c5f3563b8d6a1d3534af7e5
Bulrush3 <71919632+Bulrush3@users.noreply.github.com>
2024-04-10 20:21:52 +0500

    Update trainer.py (change loss func to Jaccard)

--- trainer.py
+++ trainer.py
@@ -175,12 +175,12 @@ class SegmentationTrainer:
 
             train_logs = self._train_epoch.run(self._train_loader)
             writer.add_scalar('Accuracy/train', train_logs['iou_score'], i)
-            writer.add_scalar('Loss/train', train_logs['dice_loss'], i)
+            writer.add_scalar('Loss/train', train_logs['_loss'], i)
 
             if not self.use_only_add_val:  # если в списке есть основной val набор считаем iou по нему
                 valid_logs = self._valid_epoch.run(self.valid_loader['set'])
                 writer.add_scalar(self.valid_loader['name'] + ' iou', valid_logs['iou_score'], i)
-                writer.add_scalar(self.valid_loader['name'] + ' loss', valid_logs['dice_loss'], i)
+                writer.add_scalar(self.valid_loader['name'] + ' loss', valid_logs['jaccard_loss'], i)
                 val_iou = valid_logs['iou_score']
                 if self.valid_loader_list is not None:
                     mean_val_iou += valid_logs['iou_score'] * self.valid_loader['weight']
@@ -191,7 +191,7 @@ class SegmentationTrainer:
                     for loader in self.valid_loader_list:
                         valid_logs = self._valid_epoch.run(loader['set'])
                         writer.add_scalar(loader['name'] + ' iou', valid_logs['iou_score'], i)
-                        writer.add_scalar(loader['name'] + ' loss', valid_logs['dice_loss'], i)
+                        writer.add_scalar(loader['name'] + ' loss', valid_logs['jaccard_loss'], i)
                         mean_val_iou += valid_logs['iou_score'] * loader['weight']
 
             # считаем либо только по основному набору либо только по доп. наборам
